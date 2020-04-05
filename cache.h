3fdf247d868e1961b03632d7d3cf129c77708a5c
prakarsh <prakarshparashar@gmail.com>
2020-04-05 09:13:20 +0530

    small documentation change in cache.h

--- cache.h
+++ cache.h
@@ -95,12 +95,32 @@ int updateL1Cache(int setIndex, int tag, bool write, int data, bool dataCache);
 int writeL1Cache(int setIndex, int tag, int data, bool dataCache); 
 
 
+
+
+
 // L2 Cache
-void initL2Cache(); // Initialises the cache.
-void printL2Cache(int setIndex); // Prints the cache in its current state.
-int getLruIndexL2Cache(int setIndex); // Returns the way index of the LRU way.
-void updateLruL2Cache(int setIndex, int wayIndex); // Updates the LRU counts.
-int searchL2Cache(int setIndex, int tag); // Returns data if hit, else -1.
-int getFirstInvalidWayL2Cache(int setIndex); // Returns the first invalid way's index, else -1.
-int updateL2Cache(int setIndex, int tag, bool write, int data); // Finds a way in that index of the cache to store the new data.
-int writeL2Cache(int setIndex, int tag, int data); // Writes data onto that cache entry.
+
+
+// Initialises the cache, valid-invalid bit set to 0
+void initL2Cache(); 
+
+// Prints the cache in its current state.
+void printL2Cache(int setIndex); 
+
+// Returns the LRU way index for a particular set
+int getLruIndexL2Cache(int setIndex); 
+
+// Updates the LRU of given setIndex and wayIndex
+void updateLruL2Cache(int setIndex, int wayIndex); 
+
+// Searches for matching tag in the set, returns data if hit, else -1, index 0 for instruction cache 1 for dataCache. Also updates the LRU if tag matches
+int searchL2Cache(int setIndex, int tag); 
+
+// Returns the first invalid way's index if present, else returns -1.
+int getFirstInvalidWayL2Cache(int setIndex); 
+
+// Finds a way in that index of the cache to store the new data, replaces that block with the new block
+int updateL2Cache(int setIndex, int tag, bool write, int data); 
+
+// Writes data onto that cache entry, if tag matches, if no tag matches then returs a TAG_NO_MATCH error, if the instruction is read returns ERROR_CANNOT_WRITE_IN_INST_CACHE. The function also updates the LRU of the corresponding set if tag matches.
+int writeL2Cache(int setIndex, int tag, int data);
